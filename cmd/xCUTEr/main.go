5675825efd0f15e9a2156f26d95a83490da6b619
Niklas Wolber <niklas.wolber@octopost.eu>
2016-08-14 10:22:07 +0200

    Include missing files

---
+++ cmd/xCUTEr/main.go
@@ -0,0 +1,39 @@
+// Copyright (c) 2016 Niklas Wolber
+// This file is licensed under the MIT license.
+// See the LICENSE file for more information.
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+
+	_ "net/http/pprof"
+
+	"github.com/nwolber/xCUTEr"
+)
+
+func main() {
+	go func() {
+		log.Println(http.ListenAndServe("localhost:6060", nil))
+	}()
+
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt, os.Kill)
+
+	x := xCUTEr.New(config())
+	x.Start()
+
+	select {
+	case s := <-signals:
+		fmt.Println("Got signal:", s)
+		x.Stop()
+		x.Cancel()
+		break
+	}
+
+	log.Println("fin")
+}
