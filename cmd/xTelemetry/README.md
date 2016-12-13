78812391422c3ed7f4a1ddec6e0fa40cd0c92882
SamBeyond <sam.beyond+github@live.com>
2016-12-13 20:27:23 +0100

    Add test application for Telemetry package

---
+++ cmd/xTelemetry/README.md
@@ -0,0 +1,1 @@
+This directory contains a test application for the Telemetry package. It is _not_ automatically built by the *build* script in the root directory of the repository.
---
+++ cmd/xTelemetry/main.go
@@ -0,0 +1,69 @@
+// Copyright (c) 2016 Niklas Wolber
+// This file is licensed under the MIT license.
+// See the LICENSE file for more information.
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/nwolber/xCUTEr/flunc"
+	"github.com/nwolber/xCUTEr/job"
+	"github.com/nwolber/xCUTEr/telemetry"
+)
+
+func main() {
+	log.SetFlags(log.Lshortfile | log.Ltime | log.Ldate)
+	file := flags()
+
+	job.InitializeSSHClientStore(10 * time.Minute)
+
+	config, err := job.ReadConfig(file)
+	if err != nil {
+		log.Fatalln(file, err)
+	}
+
+	events := make(chan telemetry.Event)
+	builder := telemetry.NewTelemetryBuilder(events)
+	f, err := job.VisitConfig(builder, config)
+	if err != nil {
+		log.Fatalln("error instrumenting execution tree:", err)
+	}
+
+	var store []telemetry.Event
+	done := make(chan struct{})
+
+	go telemetry.StoreEvents(&store, events, done)
+	f.(flunc.Flunc)(context.TODO())
+	<-time.After(time.Second)
+	close(events)
+	<-done
+
+	tree, err := telemetry.NewVisualization(config)
+	if err != nil {
+		log.Fatalln("error building visualization tree:", err)
+	}
+	tree.ApplyStore(store)
+	fmt.Println(tree)
+
+	timing, err := telemetry.NewTiming(config)
+	if err != nil {
+		log.Fatalln("error building timing tree:", err)
+	}
+	timing.ApplyStore(store)
+
+	fmt.Printf("Total runtime: %s\n", timing.JobRuntime)
+	for name, timings := range timing.Hosts {
+		fmt.Printf("%s: %s\n", name, timings.Runtime)
+	}
+}
+
+func flags() (file string) {
+	flag.Parse()
+	file = flag.Arg(0)
+	return
+}
